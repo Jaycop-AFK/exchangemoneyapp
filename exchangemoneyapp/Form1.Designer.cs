307a8eadd07899bde5caea3d91aaeaf8162947de
344_14 <344_14@344_14>
2024-09-21 14:48:39 +0700

    Add project files.

---
+++ exchangemoneyapp/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace exchangemoneyapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picus = new System.Windows.Forms.PictureBox();
+            this.piceng = new System.Windows.Forms.PictureBox();
+            this.piceuro = new System.Windows.Forms.PictureBox();
+            this.picND = new System.Windows.Forms.PictureBox();
+            this.picCN = new System.Windows.Forms.PictureBox();
+            this.LDL = new System.Windows.Forms.Label();
+            this.LUS = new System.Windows.Forms.Label();
+            this.LER = new System.Windows.Forms.Label();
+            this.LND = new System.Windows.Forms.Label();
+            this.LCN = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piceng)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piceuro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picND)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picus
+            // 
+            this.picus.BackColor = System.Drawing.Color.Transparent;
+            this.picus.Image = ((System.Drawing.Image)(resources.GetObject("picus.Image")));
+            this.picus.Location = new System.Drawing.Point(68, 243);
+            this.picus.Name = "picus";
+            this.picus.Size = new System.Drawing.Size(82, 84);
+            this.picus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picus.TabIndex = 1;
+            this.picus.TabStop = false;
+            this.picus.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.picus.MouseLeave += new System.EventHandler(this.picus_MouseLeave);
+            this.picus.MouseHover += new System.EventHandler(this.picus_MouseHover);
+            // 
+            // piceng
+            // 
+            this.piceng.BackColor = System.Drawing.Color.Transparent;
+            this.piceng.Image = ((System.Drawing.Image)(resources.GetObject("piceng.Image")));
+            this.piceng.Location = new System.Drawing.Point(177, 243);
+            this.piceng.Name = "piceng";
+            this.piceng.Size = new System.Drawing.Size(82, 84);
+            this.piceng.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piceng.TabIndex = 2;
+            this.piceng.TabStop = false;
+            this.piceng.Click += new System.EventHandler(this.piceng_Click);
+            this.piceng.MouseLeave += new System.EventHandler(this.piceng_MouseLeave);
+            this.piceng.MouseHover += new System.EventHandler(this.piceng_MouseHover);
+            // 
+            // piceuro
+            // 
+            this.piceuro.BackColor = System.Drawing.Color.Transparent;
+            this.piceuro.Image = ((System.Drawing.Image)(resources.GetObject("piceuro.Image")));
+            this.piceuro.Location = new System.Drawing.Point(285, 243);
+            this.piceuro.Name = "piceuro";
+            this.piceuro.Size = new System.Drawing.Size(82, 84);
+            this.piceuro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.piceuro.TabIndex = 3;
+            this.piceuro.TabStop = false;
+            this.piceuro.Click += new System.EventHandler(this.piceuro_Click);
+            this.piceuro.MouseLeave += new System.EventHandler(this.piceuro_MouseLeave);
+            this.piceuro.MouseHover += new System.EventHandler(this.piceuro_MouseHover);
+            // 
+            // picND
+            // 
+            this.picND.BackColor = System.Drawing.Color.Transparent;
+            this.picND.Image = ((System.Drawing.Image)(resources.GetObject("picND.Image")));
+            this.picND.Location = new System.Drawing.Point(400, 243);
+            this.picND.Name = "picND";
+            this.picND.Size = new System.Drawing.Size(82, 84);
+            this.picND.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picND.TabIndex = 4;
+            this.picND.TabStop = false;
+            this.picND.Click += new System.EventHandler(this.picND_Click);
+            this.picND.MouseLeave += new System.EventHandler(this.picND_MouseLeave);
+            this.picND.MouseHover += new System.EventHandler(this.picND_MouseHover);
+            // 
+            // picCN
+            // 
+            this.picCN.BackColor = System.Drawing.Color.Transparent;
+            this.picCN.Image = ((System.Drawing.Image)(resources.GetObject("picCN.Image")));
+            this.picCN.Location = new System.Drawing.Point(513, 243);
+            this.picCN.Name = "picCN";
+            this.picCN.Size = new System.Drawing.Size(82, 84);
+            this.picCN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picCN.TabIndex = 5;
+            this.picCN.TabStop = false;
+            this.picCN.Click += new System.EventHandler(this.picCN_Click);
+            this.picCN.MouseLeave += new System.EventHandler(this.picCN_MouseLeave);
+            this.picCN.MouseHover += new System.EventHandler(this.picCN_MouseHover);
+            // 
+            // LDL
+            // 
+            this.LDL.AutoSize = true;
+            this.LDL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LDL.Location = new System.Drawing.Point(64, 189);
+            this.LDL.Name = "LDL";
+            this.LDL.Size = new System.Drawing.Size(89, 20);
+            this.LDL.TabIndex = 6;
+            this.LDL.Text = "....................";
+            // 
+            // LUS
+            // 
+            this.LUS.AutoSize = true;
+            this.LUS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LUS.Location = new System.Drawing.Point(173, 189);
+            this.LUS.Name = "LUS";
+            this.LUS.Size = new System.Drawing.Size(89, 20);
+            this.LUS.TabIndex = 7;
+            this.LUS.Text = "....................";
+            this.LUS.Click += new System.EventHandler(this.LUS_Click);
+            // 
+            // LER
+            // 
+            this.LER.AutoSize = true;
+            this.LER.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LER.Location = new System.Drawing.Point(281, 189);
+            this.LER.Name = "LER";
+            this.LER.Size = new System.Drawing.Size(89, 20);
+            this.LER.TabIndex = 8;
+            this.LER.Text = "....................";
+            // 
+            // LND
+            // 
+            this.LND.AutoSize = true;
+            this.LND.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LND.Location = new System.Drawing.Point(396, 189);
+            this.LND.Name = "LND";
+            this.LND.Size = new System.Drawing.Size(89, 20);
+            this.LND.TabIndex = 9;
+            this.LND.Text = "....................";
+            // 
+            // LCN
+            // 
+            this.LCN.AutoSize = true;
+            this.LCN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LCN.Location = new System.Drawing.Point(509, 189);
+            this.LCN.Name = "LCN";
+            this.LCN.Size = new System.Drawing.Size(89, 20);
+            this.LCN.TabIndex = 10;
+            this.LCN.Text = "....................";
+            this.LCN.MouseLeave += new System.EventHandler(this.LCN_MouseLeave);
+            this.LCN.MouseHover += new System.EventHandler(this.LCN_MouseHover);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 26);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Exchange Your THB";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 373);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LCN);
+            this.Controls.Add(this.LND);
+            this.Controls.Add(this.LER);
+            this.Controls.Add(this.LUS);
+            this.Controls.Add(this.LDL);
+            this.Controls.Add(this.picCN);
+            this.Controls.Add(this.picND);
+            this.Controls.Add(this.piceuro);
+            this.Controls.Add(this.piceng);
+            this.Controls.Add(this.picus);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exchange Your Money";
+            ((System.ComponentModel.ISupportInitialize)(this.picus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piceng)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piceuro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picND)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picus;
+        private System.Windows.Forms.PictureBox piceng;
+        private System.Windows.Forms.PictureBox piceuro;
+        private System.Windows.Forms.PictureBox picND;
+        private System.Windows.Forms.PictureBox picCN;
+        private System.Windows.Forms.Label LDL;
+        private System.Windows.Forms.Label LUS;
+        private System.Windows.Forms.Label LER;
+        private System.Windows.Forms.Label LND;
+        private System.Windows.Forms.Label LCN;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ exchangemoneyapp/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exchangemoneyapp
+{
+    public partial class Form1 : Form
+    {
+        double bath;
+        double dollar = 36.5176;
+        double pond = 46.0846;
+        double euro = 38.8960;
+        double xaun = 4.9790;
+        double rupe = 0.4090;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            bath = Convert.ToDouble(textBox1.Text);
+            double exchange = bath / dollar;
+            LDL.Text = exchange.ToString("#.####");
+            
+           // LDL.Text = Convert.ToString(bath / dollar);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picus_MouseLeave(object sender, EventArgs e)
+        {
+            LDL.Text = "....................";
+        }
+
+        private void picus_MouseHover(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                bath = Convert.ToDouble(textBox1.Text);
+                double exchange = bath / dollar;
+                LDL.Text = exchange.ToString("#.####");
+            }
+        }
+
+        private void piceng_Click(object sender, EventArgs e)
+        {
+            bath = Convert.ToDouble(textBox1.Text);
+            double exchange = bath / pond;
+            LUS.Text = exchange.ToString("#.####");
+        }
+
+        private void piceng_MouseHover(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                bath = Convert.ToDouble(textBox1.Text);
+                double exchange = bath / pond;
+                LUS.Text = exchange.ToString("#.####");
+            }
+        }
+
+        private void piceng_MouseLeave(object sender, EventArgs e)
+        {
+            LUS.Text = "....................";
+        }
+
+        private void piceuro_Click(object sender, EventArgs e)
+        {
+            bath = Convert.ToDouble(textBox1.Text);
+            double exchange = bath / euro;
+            LER.Text = exchange.ToString("#.####");
+        }
+
+        private void piceuro_MouseHover(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                bath = Convert.ToDouble(textBox1.Text);
+                double exchange = bath / euro;
+                LER.Text = exchange.ToString("#.####");
+            }
+        }
+
+        private void piceuro_MouseLeave(object sender, EventArgs e)
+        {
+            LER.Text = "....................";
+        }
+
+        private void picND_MouseHover(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") {
+            bath = Convert.ToDouble(textBox1.Text);
+            double exchange = bath / rupe;
+            LND.Text = exchange.ToString("#.####");
+            }
+        }
+
+        private void picND_MouseLeave(object sender, EventArgs e)
+        {
+            LND.Text = "....................";
+        }
+
+        private void picND_Click(object sender, EventArgs e)
+        {
+            bath = Convert.ToDouble(textBox1.Text);
+            double exchange = bath / rupe;
+            LND.Text = exchange.ToString("#.####");
+        }
+
+        private void LCN_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void picCN_Click(object sender, EventArgs e)
+        {
+            bath = Convert.ToDouble(textBox1.Text);
+            double exchange = bath / xaun;
+            LCN.Text = exchange.ToString("#.####");
+        }
+
+        private void LCN_MouseLeave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void picCN_MouseHover(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                bath = Convert.ToDouble(textBox1.Text);
+                double exchange = bath / xaun;
+                LCN.Text = exchange.ToString("#.####");
+            }
+        }
+
+        private void picCN_MouseLeave(object sender, EventArgs e)
+        {
+            LCN.Text = "....................";
+        }
+
+        private void LUS_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
